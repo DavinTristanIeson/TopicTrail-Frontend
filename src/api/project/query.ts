51879720e58961a1a67e72d8ddb8ae6473d2f396
Hansen Tanio <hansentanio33@gmail.com>
2024-10-30 22:38:04 +0700

    feat: add query for page and limit

--- src/api/project/query.ts
+++ src/api/project/query.ts
@@ -5,25 +5,33 @@ import { useQuery } from "@tanstack/react-query";
 import { ApiFetch } from "@/common/api/fetch";
 import { StaleTimes } from "../common/query";
 import { ApiResult, ExtendedApiResult } from "@/common/api/model";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 export const ProjectQueryKeys = {
   listKey: "getProjects",
   detailKey: "getProject",
   detail(input: IdInput) {
     return [ProjectQueryKeys.detailKey, input.id];
+  },
+  list(input: { page: number; limit: number }) { // Add this function
+    return [ProjectQueryKeys.listKey, { page: input.page, limit: input.limit }];
   }
 }
 
 const ENDPOINT = "projects";
 
 export const useGetProjects: ApiQueryFunction<void, ExtendedApiResult<ProjectLiteModel[]>> = function (input, options) {
+  const location = useLocation();
+  const { page = 1, limit = 10 } = queryString.parse(location.search);
+
   return useQuery({
-    queryKey: [ProjectQueryKeys.listKey],
+    queryKey: ProjectQueryKeys.list({ page: Number(page), limit: Number(limit) }),
     staleTime: StaleTimes.Long,
     ...options,
     queryFn() {
       return ApiFetch({
-        url: ENDPOINT,
+        url: `${ENDPOINT}?page=${page}&limit=${limit}`,
         classType: ProjectLiteModel,
         method: 'get',
       });
