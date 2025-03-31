85d948c8dc9fc238a30eae5eb47e22748acee3b8
Angeline Ho <angelineho16.03@gmail.com>
2025-03-31 15:31:34 +0700

    feat: rediscover topic dan redirect refine topic btn

--- src/modules/topics/results/controls.tsx
+++ src/modules/topics/results/controls.tsx
@@ -4,8 +4,10 @@ import { TopicModelingOptionFlagCheckboxes } from '../components/controls';
 import { ProjectContext, SchemaColumnContext } from '@/modules/project/context';
 import { invalidateProjectDependencyQueries } from '@/api/project';
 import { ArrowClockwise } from '@phosphor-icons/react';
-import { Button, Modal } from '@mantine/core';
+import { Text, Group, Button, Modal, Box, Stack, Tooltip } from '@mantine/core';
 import { DisclosureTrigger, useDisclosureTrigger } from '@/hooks/disclosure';
+import { useRouter } from 'next/router';
+import NavigationRoutes from '@/common/constants/routes';
 
 const RediscoverTopicsModal = React.forwardRef<DisclosureTrigger | null>(
   function RediscoverTopicsModal(props, ref) {
@@ -20,37 +22,60 @@ const RediscoverTopicsModal = React.forwardRef<DisclosureTrigger | null>(
     const onStart = React.useCallback(async () => {
       await onStartTopicModeling();
       invalidateProjectDependencyQueries(project.id);
+      close();
     }, [onStartTopicModeling, project.id]);
 
     return (
-      <Modal opened={opened} onClose={close}>
+      <Modal opened={opened} onClose={close} title="Re-discover Topics">
+        <Text mb={20}>
+          Are you sure you want to run the topic model again? This will clear the topic information.
+        </Text>
         <TopicModelingOptionFlagCheckboxes {...startActions} />
+        <Group mt="md" pr="md" style={{ justifyContent: 'flex-end' }}>
+          <Button variant="outline" onClick={close}>Cancel</Button>
+          <Button onClick={onStart}>Confirm</Button>
+        </Group>
       </Modal>
     );
   },
 );
 
 export default function TopicResultsPageControls() {
+  const router = useRouter();
   const project = React.useContext(ProjectContext);
   const modalRemote = React.useRef<DisclosureTrigger | null>(null);
 
-  // TODO: Angeline
-  // Requirements
-  // - Tombol start pemodelan topik
-  // - Tombol ke halaman Refine Topics
-  // - Komponen ini hover di bagian kanan layar. Tidak usah ikut scroll
-  // - Modal konfirmasi ketika tekan Rediscover Topics. Taruh TopicModelingOptionFlagCheckboxes di dalam sana.
   return (
     <>
       <RediscoverTopicsModal ref={modalRemote} />
-      <Button
-        onClick={() => {
-          modalRemote.current?.open();
-        }}
-        leftSection={<ArrowClockwise />}
-      >
-        Re-discover Topics
-      </Button>
+      <Box style={{ backgroundColor:'white', padding: '12px', borderRadius: '8px', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', border: '1px solid #ccc'}}>
+        <Stack>
+          <Tooltip label="Go to Refine Topics Page" withArrow style={{ border: '1px solid #ccc' }}>
+            <Button
+              onClick={() => {
+                router.push({
+                  pathname: NavigationRoutes.ProjectRefineTopics,
+                  query: {
+                    id: project.id,
+                  },
+                });
+              }}
+            >
+              Refine Topic
+            </Button>
+          </Tooltip>
+          <Tooltip label="Re-run topic discovery process" withArrow style={{ border: '1px solid #ccc' }}>
+            <Button
+              onClick={() => {
+                modalRemote.current?.open();
+              }}
+              leftSection={<ArrowClockwise />}
+            >
+              Re-discover Topics
+            </Button>
+          </Tooltip>
+        </Stack>
+      </Box>
     </>
   );
 }
