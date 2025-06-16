6e02cde69878a4bfd7789f68d789f7bcfb984286
Davin Tristan Ieson <davin.tristan@gmail.com>
2025-06-16 19:55:56 +0700

    chore: README and LICENSE
    refine: default API endpoint (in prod) is the origin

--- src/common/constants/global.ts
+++ src/common/constants/global.ts
@@ -2,5 +2,13 @@ const GlobalConfig = {
   ApiUrl: 'http://localhost:8000/api',
   AppName: 'TopicTrail',
 };
+const isDev = process.env.NODE_ENV === 'development';
+if (typeof window !== 'undefined') {
+  window.onload = function () {
+    if (!isDev) {
+      GlobalConfig.ApiUrl = `${window.location.origin}/api`;
+    }
+  };
+}
 
 export default GlobalConfig;
